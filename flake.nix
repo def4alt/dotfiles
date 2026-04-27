67aa6f61c24f1cefe26b51f29f097c0c555a6373
Andrii Olkhovych <andrii.olkhovych@tum.de>
2026-04-27 16:05:22 +0200

    chore(flake): update inputs and helper exports

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "Def4alt's Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
--- lib/default.nix
+++ lib/default.nix
@@ -10,5 +10,6 @@ in {
     (helpers)
     mkDarwin
     mkHome
+    forAllSystems
     ;
 }
